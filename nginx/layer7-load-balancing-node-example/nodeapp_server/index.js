69359024999fc835c20e9e48c6bfcf0979693962
Andrew Zang <andrewz020529@gmail.com>
2023-02-14 21:14:43 -0500

    nginx block path and path redirection

--- nginx/layer7-load-balancing-node-example/nodeapp_server/index.js
+++ nginx/layer7-load-balancing-node-example/nodeapp_server/index.js
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
   res.send(`Hello from APPID ${process.env.APPID}`)
 })
 
+app.get('/admin', (req, res) => {
+  res.send(`APPID: ${process.env.APPID}, this is an admin path`)
+})
+
 app.listen(9999)
